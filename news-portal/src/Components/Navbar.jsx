081b6721647966c49d8e3852a419b376658b4a40
Dewa0000 <dewashishsahu8888@gmail.com>
2025-05-22 11:52:07 +0530

    Second Commit

--- news-portal/src/Components/Navbar.jsx
+++ news-portal/src/Components/Navbar.jsx
@@ -1,11 +1,39 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+ 
+  const [searchItem, setSearchItem] = useState("");
+  const navigate = useNavigate();
+
+  function handleSearchItem(e){
+     e.preventDefault();
+     if(searchItem.trim()){
+        navigate(`/search?q = ${searchItem.trim()} `)
+     setSearchItem("");
+     }
+     
+  }
+
   return (
     <nav className="bg-blue-700 text-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between">
         <Link to="/" className="text-xl font-bold">ByteNews</Link>
-        <div className="space-x-4">
+        <div  className="flex items-center space-x-4">
+          <div className="searchContainer flex">
+            <form onSubmit={handleSearchItem} role="search" id="form" className="flex items-center">
+              <input
+                type="search"
+                id="query"
+                value={searchItem}
+                name="q"
+                onChange={() => setSearchItem(e.target.value)}
+                placeholder="Search..."
+                className="px-2 py-1 rounded text-black"
+              />
+              <button type="submit" className="ml-2 px-3 py-1 bg-white text-blue-700 rounded">Go</button>
+            </form>
+          </div>
           <Link to="/" className="hover:underline">Home</Link>
           <Link to="/tech" className="hover:underline">Tech</Link>
           <Link to="/startups" className="hover:underline">Startups</Link>
