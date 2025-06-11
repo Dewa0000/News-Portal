61afbfedc26ec43f5fe5385ed0493f8ce54616ed
Dewa0000 <dewashishsahu8888@gmail.com>
2025-06-11 15:10:14 +0530

    35th Commit

--- news-portal/src/Components/searchResults.jsx
+++ news-portal/src/Components/searchResults.jsx
@@ -10,7 +10,7 @@ const SearchResults = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://news-portal-jzcd.onrender.com";
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     fetch(`${backendUrl}/blogs`)
       .then(res => res.json())
       .then(data => {
