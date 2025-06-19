853191c36e331a486da4671244c86f1165724aa5
Dewa0000 <dewashishsahu8888@gmail.com>
2025-06-19 16:13:32 +0530

    92nd

--- news-portal/src/Pages/Jobs.jsx
+++ news-portal/src/Pages/Jobs.jsx
@@ -4,11 +4,21 @@ import Header from "../Components/Header";
 
 const Jobs = () => {
     
-    const {news : jobCard, loading} = useFetchNews("jobs")
+    const {news: jobRoles,loading} = useFetchNews("jobs")
 
     return (
            <div>
               <Header/>
+              {loading ?
+                 (<p className="text-center">Loading...</p>) : 
+                 jobRoles.length === 0 ? 
+                 (<p>No Job Role Found</p>) :
+                 (<div>
+                    {jobRoles.map((item) => {
+                    return <NewsCard blog={item} key={item._id}/>
+                 })}
+                 </div>)
+              }
            </div>
     )
 }
