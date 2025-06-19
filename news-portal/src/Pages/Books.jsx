efdd98a39d625969d7564b053e8e1bcd8b5a08d5
Dewa0000 <dewashishsahu8888@gmail.com>
2025-06-19 14:10:03 +0530

    90th

--- news-portal/src/Pages/Books.jsx
+++ news-portal/src/Pages/Books.jsx
@@ -3,24 +3,24 @@ import NewsCard from "../Components/NewsCard";
 import Header from "../Components/Header";
 
 const Books = () => {
-   
-    const {news: bookNews, loading} = useFetchNews("books")
+  const { news: bookNews, loading } = useFetchNews("books");
 
+  return (
+    <div>
+      <Header />
+      {loading ? (
+        <p className="text-center">Loading...</p>
+      ) : bookNews.length === 0 ? (
+        <p className="font-bold text-center text-red-500">No Books Blog Found</p>
+      ) : (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {bookNews.map((item) => (
+            <NewsCard blog={item} key={item._id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-
-    return (
-         <div>
-            <Header/>
-            {loading ? 
-            <p className="text-center">Loading...</p> : 
-               bookNews.length === 0 ? 
-            <p className="font-bold">No Books Blog Found</p> : 
-            <div>{bookNews.map((item) => {
-                 return <NewsCard blog={item} key={item._id}/>
-            })}
-                
-            </div> }
-          </div>
-
-    )}
 export default Books;
