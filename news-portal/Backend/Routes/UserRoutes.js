98ce47a56c00fd2686f616052807802144198e9f
Dewa0000 <dewashishsahu8888@gmail.com>
2025-06-20 21:56:32 +0530

    102nd

--- news-portal/Backend/Routes/UserRoutes.js
+++ news-portal/Backend/Routes/UserRoutes.js
@@ -16,7 +16,7 @@ router.post("/signup", async (req,res) => {
           const user = await User.signup(email,password);
           res.status(200).json({email,user});
       }catch(error){
-          res.status(400).json({error: error.msg})
+          res.status(400).json({error: error.message})
       }
       res.json({msg: "signup User"})
 })
